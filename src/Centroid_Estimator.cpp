5ea993ebad1ed14ac6b2915b6d988559b16675f6
Dan Mangles <florian.jaeckle@gmail.com>
2019-02-08 14:25:35 +0000

    Updated Centroid_Estimator
    Structure changes with how we pass information around.
    
    Doesn't entirely work

--- src/Centroid_Estimator.cpp
+++ src/Centroid_Estimator.cpp
@@ -1,44 +1,26 @@
-/*
- * Centroid_Estimator.cpp
- *
- *  Created on: 6 Feb 2019
- *      Author: dan
- */
-
-// sensor msgs includes specific includes
-#ifndef Centroid_Estimator_H
-#define Centroid_Estimator_H
-
-#include <ros/ros.h> // because this is a robot
 
 
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointCloud.h>
 
-#include <vector>
 
 #include "include/Centroid_Estimator.h"
 
-#include <iostream>
-#include <stdexcept>
 
 using namespace Eigen;
 using namespace std;
 
+//ros::NodeHandle nh;
 
 Centroid_Estimator::Centroid_Estimator(
-        int init_variable)
+        int init_variable)//, ros::NodeHandle &nh)
         : init_variable_(init_variable) {
+
     cout <<"this is the constructor" <<endl;
     // subscribe to pcl and use a callback function to execute stuff
 
-//    string input_topic = "/velodyne/point_cloud_filtered";
-//    ros::Subscriber sub = nh_.subscribe<sensor_msgs::PointCloud2> (input_topic, 1,&Centroid_Estimator::cloud_cb); // subscribe to the pointcloud
-
-
-
-
-
+    string input_topic = "/velodyne/point_cloud_filtered";
+    //ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> (input_topic, 1,&Centroid_Estimator::cloud_cb); // subscribe to the pointcloud
 
 
 } // if the constructor has any init variables, need to assign them here
@@ -48,12 +30,15 @@ void Centroid_Estimator::cloud_cb(const sensor_msgs::PointCloud2ConstPtr &cloud_
     // INPUTS: cloud_msg: pointer to the raw input cloud from the velodyne
         // This is a callback function that reads in a pointcloud, gets velodyne coords, updates kalman filter
 
+    // for each pcl that comes in:
 
+    //ros::NodeHandle nh;
+    publish_cluster_centroid(cloud_msg);
 
 }
 
 
-void Centroid_Estimator::generate_cluster_array (pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud, vector <pcl::PointCloud<pcl::PointXYZ>::Ptr> centroid_cluster_array) {
+void Centroid_Estimator::publish_cluster_centroid (const sensor_msgs::PointCloud2ConstPtr cloud_msg) {
   // INPUTS: input_cloud: pointer to the pointcloud produced by the velodyne relative to "base"
   //          centroid_cluster_array: pointer to output cloud which contains the clusters
   // This method takes the pcloud input_cloud, downsizes it with a voxel grid, splits up the grid into
@@ -61,17 +46,11 @@ void Centroid_Estimator::generate_cluster_array (pcl::PointCloud<pcl::PointXYZ>:
 
     cout <<"executing method generate_cluster_array"<<endl;
 
-}
-
-void Centroid_Estimator::update_centroid_coords(const sensor_msgs::PointCloud2ConstPtr &cloud_msg) {
-        //process the pointcloud message and return a vector of coordinates
-
-        cout << "executing method update_centroid_coords"<<endl;
-
-        // Publish the cloud
-        //pub_raw.publish (*cloud_msg); // publish the raw cloud
-
+    // filter the pointcloud
+    // cluster it
+    // for each cluster
+    //       get the coordinates
+    //       add to coordinate vector
+    //       publish to a transform
 
 }
-
-#endif // makes sure the ifndef works to prevent repeat class definitions
--- src/include/Centroid_Estimator.h
+++ src/include/Centroid_Estimator.h
@@ -9,8 +9,13 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <ros/ros.h> // because this is a robot
 
+#include <ros/ros.h> // because this is a robot
+#include <ros/console.h>
+
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #ifndef CENTROID_ESTIMATOR_H
 #define CENTROID_ESTIMATOR_H
@@ -20,23 +25,20 @@ using namespace std;
 
 class Centroid_Estimator {
 
+
+
 public:
         // all public methods and variables go here
-        Centroid_Estimator(
-                int init_variable
-                ); // could initiate with certain variables but at this point, hardcode everything into the object
-
-
-
-        void update_centroid_coords(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+        Centroid_Estimator(int init_variable); // could initiate with certain variables but at this point, hardcode everything into the object
 
 private:
-
-         // Matrices for computation
-        void generate_cluster_array (pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud, vector <pcl::PointCloud<pcl::PointXYZ>::Ptr> centroid_cluster_array);
+        void publish_cluster_centroid (const sensor_msgs::PointCloud2ConstPtr cloud_msg);
         void cloud_cb(const sensor_msgs::PointCloud2ConstPtr &cloud_msg);
         int init_variable_; // define the centroid publisher here
-       // ros::NodeHandle nh_; // internal copy of the node handle
+
+//        ros::NodeHandle nh_; // internal copy of the node handle
+
+
 };
 
 #endif
