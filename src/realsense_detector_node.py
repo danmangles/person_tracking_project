ce350712d39f479637ff22c9f2c784220c2ee206
Dan Mangles <florian.jaeckle@gmail.com>
2019-03-04 11:45:44 +0000

    Restructured into a combined callback
    combined_callback() now processes both depth and rgb info

--- src/realsense_detector_node.py
+++ src/realsense_detector_node.py
@@ -6,9 +6,10 @@ import realsense_detector
 
 def main():
 
-    rospy.init_node('realsense_person_detector', anonymous=True)
+    r = rospy.init_node('realsense_person_detector', anonymous=True)
     rd = realsense_detector.RealsenseDetector(in_simulator = False, verbose = True) # create a new realsense detector
 
+    # rate = rospy.Rate(2) # set rate to 2 Hz
     rospy.spin()
 
 main()
